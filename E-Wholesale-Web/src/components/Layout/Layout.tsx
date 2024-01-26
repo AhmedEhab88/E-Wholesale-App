e546e9e3a629d2ea9d677b68564984fb560ab3ae
AhmedEhab88 <ahmedesrm@gmail.com>
2024-01-26 23:27:36 +0200

    Created layout component for nav bar

---
+++ E-Wholesale-Web/src/components/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+import style from './Navbar.module.css';
+
+function Layout() {
+    return (
+        <div className={style.topnav}>
+            <div className={style.logo}>E-WholeSale</div>
+
+            <a className={style.active} href="#home">
+                Home
+            </a>
+            <a href="#inventory">Inventory</a>
+            <a href="#users">Users</a>
+            <a href="#retailers">Retailers</a>
+
+            <Outlet />
+        </div>
+    );
+}
+
+export default Layout;
