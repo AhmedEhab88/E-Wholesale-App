73d6223e837756663140dc985a5e6cb690d11ea8
أحمد محمود <amhmoud@moj.gov.sa>
2024-02-27 23:29:33 +0300

    Added ion-icons to summaries component

--- E-Wholesale-Web/src/components/Summaries/Summaries.tsx
+++ E-Wholesale-Web/src/components/Summaries/Summaries.tsx
@@ -1,3 +1,5 @@
+import { TimeOutline } from 'react-ionicons';
+
 interface SummariesProps {
     title: string;
     value: number;
@@ -5,7 +7,13 @@ interface SummariesProps {
 
 function Summaries({ title, value }: SummariesProps) {
     return (
-        <div className="shadow-xl my-5 border-2 border-purple-300 w-[30%] h-36 rounded flex flex-col items-center">
+        <div className="shadow-xl my-5 border-2 border-purple-300 w-[30%] h-36 rounded-xl flex flex-col items-center gap-3">
+            <TimeOutline
+                color={'#00000'}
+                title={'Time'}
+                height="50px"
+                width="50px"
+            />
             <p>{title}</p>
             <p>{value}</p>
         </div>
