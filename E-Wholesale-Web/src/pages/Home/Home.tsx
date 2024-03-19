c9468402f951dfcc5389812c65b6fb54621568c3
أحمد محمود <amhmoud@moj.gov.sa>
2024-03-19 14:22:06 +0300

    Fixed styling in home page

--- E-Wholesale-Web/src/pages/Home/Home.tsx
+++ E-Wholesale-Web/src/pages/Home/Home.tsx
@@ -3,16 +3,20 @@ import Summaries from '../../components/Summaries/Summaries';
 
 function Home() {
     const items = [
-        { title: 'Revenue Generated', value: 150 },
-        { title: 'Retailers', value: 26 },
+        { title: 'Total Revenue Generated', value: '$1502.58' },
+        { title: 'Total Retailers Registered', value: 53 },
+        { title: 'Total Representatives', value: 26 },
+        { title: 'Items in Inventory', value: 561 },
+        { title: 'Orders Completed', value: 205 },
+        { title: 'Pending Orders', value: 26 },
     ];
     return (
-        <div className="ml-10 mr-10 w-[90%]">
-            <h1 className="text-4xl mt-10 font-bold italic text-left text-sky-400">
-                Welcome [User]!
+        <div>
+            <h1 className="text-4xl mt-10 font-oxanium font-bold text-left text-[#004956]">
+                Welcome, [User]!
             </h1>
-            <h3 className="text-xl mt-10">
-                Here is a summary of the wholesale application...
+            <h3 className="text-xl mt-10 text-[#004956] font-oxanium capitalize">
+                Here's a summary of your wholesale shop.
             </h3>
             <div className="flex flex-row flex-wrap justify-between items-center w-full">
                 {items.map((item) => (
@@ -20,11 +24,11 @@ function Home() {
                         <TimeOutline
                             color={'#00000'}
                             title={'Time'}
-                            height="50px"
-                            width="50px"
+                            height="40px"
+                            width="40px"
                         />
                         <p>{item.title}</p>
-                        <p>{item.value}</p>
+                        <p className="text-[20px] font-medium">{item.value}</p>
                     </Summaries>
                 ))}
             </div>
