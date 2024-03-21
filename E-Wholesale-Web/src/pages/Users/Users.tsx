487a8acc482981103b497a8a2cdded26d51f51d0
أحمد محمود <amhmoud@moj.gov.sa>
2024-03-21 14:13:22 +0300

    Initial version of users page

--- E-Wholesale-Web/src/pages/Users/Users.tsx
+++ E-Wholesale-Web/src/pages/Users/Users.tsx
@@ -1,5 +1,64 @@
+import Button from '../../components/Button/Button';
+import SearchBar from '../../components/SearchBar/SearchBar';
+
 function Users() {
-    return <div>Users</div>;
+    const data = [
+        { Id: 1, Name: 'Ahmed', OrdersCompleted: 13 },
+        { Id: 2, Name: 'Omar', OrdersCompleted: 25 },
+        { Id: 3, Name: 'Mahmoud', OrdersCompleted: 20 },
+        { Id: 4, Name: 'Khalid', OrdersCompleted: 37 },
+    ];
+
+    return (
+        <>
+            <div className="mt-10 flex justify-center">
+                <h1 className="text-[40px] text-[#004956]">Users</h1>
+            </div>
+            <div className="flex justify-between items-center mt-10">
+                <SearchBar />
+                <Button>
+                    <p className="text-sm px-5">Add User</p>
+                </Button>
+            </div>
+
+            <div className="mt-10">
+                <table className="w-full border-[#004956] border-solid border">
+                    <tr>
+                        <th className="border border-[#004956] border-solid text-center">
+                            Id
+                        </th>
+                        <th className="border border-[#004956] border-solid text-center">
+                            Name
+                        </th>
+                        <th className="border border-[#004956] border-solid text-center">
+                            Orders Completed
+                        </th>
+                        <th className="border border-[#004956] border-solid text-center">
+                            Actions
+                        </th>
+                    </tr>
+                    {data.map((item) => {
+                        return (
+                            <tr>
+                                <td className="border border-[#004956] border-solid text-center">
+                                    {item.Id}
+                                </td>
+                                <td className="border border-[#004956] border-solid text-center">
+                                    {item.Name}
+                                </td>
+                                <td className="border border-[#004956] border-solid text-center">
+                                    {item.OrdersCompleted}
+                                </td>
+                                <td className="border border-[#004956] border-solid text-center">
+                                    View, Update, Delete
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </table>
+            </div>
+        </>
+    );
 }
 
 export default Users;
