c607269e9a3d26627f300909909c2db5c5ff2ba6
أحمد محمود <amhmoud@moj.gov.sa>
2024-03-25 23:51:45 +0300

    Passed onClick function as props to Button component

--- E-Wholesale-Web/src/components/Button/Button.tsx
+++ E-Wholesale-Web/src/components/Button/Button.tsx
@@ -1,10 +1,14 @@
 interface ButtonProps {
     children: React.ReactNode;
+    onClick: () => void;
 }
 
-function Button({ children }: ButtonProps) {
+function Button({ children, onClick }: ButtonProps) {
     return (
-        <button className="bg-[#D8F8F1] p-2 rounded-xl hover:bg-[#004956] hover:text-white">
+        <button
+            className="bg-[#D8F8F1] p-2 rounded-xl hover:bg-[#004956] hover:text-white"
+            onClick={onClick}
+        >
             {children}
         </button>
     );
