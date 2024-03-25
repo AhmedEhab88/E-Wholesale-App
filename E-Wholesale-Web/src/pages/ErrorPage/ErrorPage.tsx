689930cfae48b1badb9b5fb2bf9fe12f92cba8ca
أحمد محمود <amhmoud@moj.gov.sa>
2024-03-25 23:51:23 +0300

    Styled error page

--- E-Wholesale-Web/src/pages/ErrorPage/ErrorPage.tsx
+++ E-Wholesale-Web/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,5 +1,9 @@
 function ErrorPage() {
-    return <div>404 Not Found</div>;
+    return (
+        <div className="flex justify-center items-center">
+            <h1 className="text-3xl text-red-700 text-center">404 Not Found</h1>
+        </div>
+    );
 }
 
 export default ErrorPage;
