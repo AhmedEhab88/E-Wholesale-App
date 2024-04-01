dcb0190bfd9cf5d0f0dcbe0ec25df1692068cda2
أحمد محمود <amhmoud@moj.gov.sa>
2024-04-01 23:30:48 +0300

    Implemented View and Delete options

--- E-Wholesale-Web/src/pages/Users/Users.tsx
+++ E-Wholesale-Web/src/pages/Users/Users.tsx
@@ -2,15 +2,47 @@ import { useNavigate } from 'react-router-dom';
 
 import Button from '../../components/Button/Button';
 import SearchBar from '../../components/SearchBar/SearchBar';
+import ViewOption from '../../components/ViewOption/ViewOption';
+import { useState } from 'react';
+import Modal from '../../components/Modal/Modal';
+import DeleteOption from '../../components/DeleteOption/DeleteOption';
 
 function Users() {
+    const [isViewModalOpen, setIsViewModalOpen] = useState(false);
+    const [currentOpenItem, setCurrentOpenItem] = useState(-1);
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
     const navigate = useNavigate();
 
     const data = [
-        { Id: 1, Name: 'Ahmed', OrdersCompleted: 13 },
-        { Id: 2, Name: 'Omar', OrdersCompleted: 25 },
-        { Id: 3, Name: 'Mahmoud', OrdersCompleted: 20 },
-        { Id: 4, Name: 'Khalid', OrdersCompleted: 37 },
+        {
+            Id: 1,
+            Name: 'Ahmed',
+            OrdersCompleted: 13,
+            Email: 'ahmed@gmail.com',
+            PhoneNumber: '01014417069',
+        },
+        {
+            Id: 2,
+            Name: 'Omar',
+            OrdersCompleted: 25,
+            Email: 'omar@gmail.com',
+            PhoneNumber: '01014417069',
+        },
+        {
+            Id: 3,
+            Name: 'Mahmoud',
+            OrdersCompleted: 20,
+            Email: 'mahmoud@gmail.com',
+            PhoneNumber: '01014417069',
+        },
+        {
+            Id: 4,
+            Name: 'Khalid',
+            OrdersCompleted: 37,
+            Email: 'khalid@gmail.com',
+            PhoneNumber: '01014417069',
+        },
     ];
 
     return (
@@ -57,7 +89,19 @@ function Users() {
                                         {item.OrdersCompleted}
                                     </td>
                                     <td className="border border-[#004956] border-solid text-center">
-                                        View, Update, Delete
+                                        <ViewOption
+                                            onClick={() => {
+                                                setIsViewModalOpen(true);
+                                                setCurrentOpenItem(item.Id);
+                                            }}
+                                        />
+                                        , Update,
+                                        <DeleteOption
+                                            onClick={() => {
+                                                setIsDeleteModalOpen(true);
+                                                setCurrentOpenItem(item.Id);
+                                            }}
+                                        />
                                     </td>
                                 </tr>
                             );
@@ -65,6 +109,64 @@ function Users() {
                     </tbody>
                 </table>
             </div>
+
+            {isViewModalOpen && (
+                <Modal setIsOpen={setIsViewModalOpen}>
+                    <div className="m-0 p-[10px] text-[#2c3e50] font-bold text-[20  px] text-center">
+                        {data.find((item) => item.Id === currentOpenItem)!.Name}
+                    </div>
+                    <div className="p-[10px] text-[#2c3e50] text-[16px] text-center">
+                        Orders Completed:{' '}
+                        {
+                            data.find((item) => item.Id === currentOpenItem)!
+                                .OrdersCompleted
+                        }
+                    </div>
+                    <div className="p-[10px] text-[#2c3e50] text-[16px] text-center">
+                        Email:{' '}
+                        {
+                            data.find((item) => item.Id === currentOpenItem)!
+                                .Email
+                        }
+                    </div>
+                    <div className="p-[10px] text-[#2c3e50] text-[16px] text-center">
+                        Phone Number:{' '}
+                        {
+                            data.find((item) => item.Id === currentOpenItem)!
+                                .PhoneNumber
+                        }
+                    </div>
+                </Modal>
+            )}
+
+            {isDeleteModalOpen && (
+                <Modal setIsOpen={setIsDeleteModalOpen}>
+                    <div className="h-12 bg-white overflow-hidden rounded-t-2xl">
+                        <h5 className="m-0 p-[10px] text-[#2c3e50] font-medium text-[18px] text-center">
+                            Dialog
+                        </h5>
+                    </div>
+                    <div className="p-[10px] text-[#2c3e50] text-[14px] text-center">
+                        Are you sure you want to delete the item?
+                    </div>
+                    <div className="absolute bottom-[2px] mb-[10px] w-full">
+                        <div className="flex justify-around items-center">
+                            <button
+                                className="mt-2.5 cursor-pointer font-medium py-2.5 px-7 rounded-lg text-sm text-white bg-red-500 transition-all duration-300 hover:shadow-lg hover:-translate-y-1 hover:bg-red-500"
+                                onClick={() => setIsDeleteModalOpen(false)}
+                            >
+                                Delete
+                            </button>
+                            <button
+                                className="mt-2.5 cursor-pointer font-medium py-2.5 px-7 rounded-lg text-sm text-gray-700 bg-gray-200 transition-all duration-300 hover:bg-gray-300"
+                                onClick={() => setIsDeleteModalOpen(false)}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </Modal>
+            )}
         </>
     );
 }
