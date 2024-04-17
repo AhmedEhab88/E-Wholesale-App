baf1381470080c810a54c1ae1b2f1343d5116742
أحمد محمود <amhmoud@moj.gov.sa>
2024-04-17 14:31:19 +0300

    Added GetRepresentativeById method in service class

---
+++ E-Wholesale-API/EWholesale.Application/Services/Implementations/RepresentativeService.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using EWholesale.Application.Models;
+using EWholesale.Application.Services.Interfaces;
+using EWholesale.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWholesale.Application.Services.Implementations
+{
+    public class RepresentativeService : IRepresentativeService
+    {
+        private readonly IRepresentativeRepository _representativeRepository;
+        private readonly IMapper _mapper;
+        
+        public RepresentativeService(IRepresentativeRepository representativeRepository, IMapper mapper)
+        {
+            _representativeRepository = representativeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<RepresentativeDto?> GetRepresentativeById(long Id)
+        {
+            var representative = await _representativeRepository.GetRepresentativeByIdAsync(Id);
+
+            if (representative != null) {
+                var representativeDto = _mapper.Map<RepresentativeDto>(representative);
+                return representativeDto;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ E-Wholesale-API/EWholesale.Application/Services/Interfaces/IRepresentativeService.cs
@@ -0,0 +1,14 @@
+﻿using EWholesale.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWholesale.Application.Services.Interfaces
+{
+    public interface IRepresentativeService
+    {
+        Task<RepresentativeDto?> GetRepresentativeById(long Id);
+    }
+}
