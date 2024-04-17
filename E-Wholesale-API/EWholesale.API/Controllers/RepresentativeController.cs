cf8968d8ac5dea502a3c2cd039ce4c37014016a5
أحمد محمود <amhmoud@moj.gov.sa>
2024-04-17 14:31:43 +0300

    Added RepresentativeController with endpoints

---
+++ E-Wholesale-API/EWholesale.API/Controllers/RepresentativeController.cs
@@ -0,0 +1,53 @@
+﻿using EWholesale.Application;
+using EWholesale.Application.Models;
+using EWholesale.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace EWholesale.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepresentativeController : ControllerBase
+    {
+        private readonly IRepresentativeService _representativeService;
+        public RepresentativeController(IRepresentativeService representativeService)
+        {
+            _representativeService = representativeService;
+        }
+
+        [HttpGet]
+        [Route("{Id}")]
+        public async Task<IActionResult> GetRepresentativeById(long Id)
+        {
+            var result = await _representativeService.GetRepresentativeById(Id);
+            if(result == null)
+            {
+                return NotFound("Representative does not exist.");
+            }
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetRepresentatives()
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        [Route("{Id}")]
+        public async Task<IActionResult> UpdateRepresentative(long Id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("{Id}")]
+        public async Task<IActionResult> DeleteRepresentative(long Id)
+        {
+            return NoContent();
+        }
+    }
+}
