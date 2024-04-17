1d694622ccd71d8015161d4e4232c7948e708eff
أحمد محمود <amhmoud@moj.gov.sa>
2024-04-17 14:30:42 +0300

    Added RepresentativeDto

---
+++ E-Wholesale-API/EWholesale.Application/Models/RepresentativeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWholesale.Application.Models
+{
+    public class RepresentativeDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int OrdersCompleted { get; set; }
+    }
+}
