73c64b313abd4faddd6a0bc017f1d7dcc83b9188
أحمد محمود <amhmoud@moj.gov.sa>
2024-04-17 15:08:01 +0300

    Added Shared project to hold all common util classes.

---
+++ E-Wholesale-API/EWholesale.Shared/Common/InquiryResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWholesale.Shared.Common
+{
+    public class InquiryResult<T>
+    {
+        public InquiryResult(IEnumerable<T> list, int totalCount)
+        {
+            List = list;
+            TotalCount = totalCount;
+        }
+
+        public IEnumerable<T> List { get; set; }
+
+        public int? TotalCount { get; set; }
+    }
+}
